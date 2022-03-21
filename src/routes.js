1864a9888b6ab85cd008b568ee1c04ea3ced0c39
Livia Fabrin Somera <liviafabrin.somera@gmail.com>
2022-03-20 23:26:28 -0300

    routes commit

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+
+export default function AppRouter() {
+    return (
+        <main>
+            <Router>
+                <Menu />                
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao/>}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>                     
+                </Routes>
+            </Router>
+        </main>
+    );
+}
